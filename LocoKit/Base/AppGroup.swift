537f1e4d3c7944119c8e0b7b6d416fc8c8a035a0
Matt <matt@bigpaua.com>
2024-04-18 14:15:35 +0800

    add Arc Recorder

--- LocoKit/Base/AppGroup.swift
+++ LocoKit/Base/AppGroup.swift
@@ -176,12 +176,12 @@ public class AppGroup {
     // MARK: - Interfaces
 
     public enum AppName: String, CaseIterable, Codable {
-        case arcV3, arcMini, arcV4
+        case arcV3, arcMini, arcRecorder
         public var sortIndex: Int {
             switch self {
             case .arcV3: return 0
             case .arcMini: return 1
-            case .arcV4: return 2
+            case .arcRecorder: return 2
             }
         }
     }
